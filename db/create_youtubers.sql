a182517fb7b73964105cc0b1aa28da77da93ade8
Tiranoe <jungwoo9543@gmail.com>
2022-10-04 19:02:41 -0400

    sql database added

---
+++ db/create_youtubers.sql
@@ -0,0 +1,11 @@
+INSERT INTO main_app_youtuber(name, img, bio, verified_youtuber, created_at)
+    VALUES
+        ('CS Dojo', 'https://yt3.ggpht.com/ytc/AMLnZu9dupLKLe6WOebxccdC3Im4aHMfCR9M4GjydKOZ=s176-c-k-c0x00ffffff-no-rj', 'YK Sugi, the YouTuber who runs CS Dojo, used to work as a software developer at Google. Now, he shares his expertise with over 1.6 million subscribers through step-by-step tutorials on a whiteboard and easy-to-follow demonstrations. Usually writes in python and Javascript.', 't', '2016-02-25');
+
+INSERT INTO main_app_youtuber(name, img, bio, verified_youtuber, created_at)
+    VALUES
+        ('Derek Banas','https://yt3.ggpht.com/ytc/AMLnZu9EUj1l1-PdMXDI0oD3UTcMypwZrnRKBy_eAZ53TA=s176-c-k-c0x00ffffff-no-rj', 'Derek Banas begins his YouTube videos with the friendly welcome, "Well, hello internet!" The YouTuber uploads tutorials on all sorts of topics, but his main areas of expertise include programming, web design, and mobile development. Usually writes in HTML, CSS and SQL', 't', '2008-08-12');
+
+INSERT INTO main_app_youtuber(name, img, bio, verified_youtuber, created_at)
+    VALUES
+        ('freeCodeCamp','https://yt3.ggpht.com/ytc/AMLnZu9UWrGceKWaqm8AF89vuxrEt8MO3E59qOoQ785Lew=s176-c-k-c0x00ffffff-no-rj', 'The nonprofit organization freeCodeCamp offers several comprehensive, self-paced courses on coding. Many of these courses incorporate YouTube videos with instructors who walk viewers through different modules. Usually writes in Python.', 't', '2014-12-16');
--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Youtuber # import the Artist model from models.py
+# Register your models here.
+
+admin.site.register(Youtuber) # this line will add the model to the admin panel
